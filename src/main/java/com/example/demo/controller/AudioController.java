5dbe809fb668f66049a13d0706a827e4c6227f36
SZAREKAR <swanand.zarekar@capgemini.com>
2022-09-27 13:18:46 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/AudioController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public final class AudioController {
+	
+	@GetMapping("/saveSound")
+	public Process getAllLocks() throws IOException, InterruptedException{
+		
+		String command = "panako query 2.mp3";
+
+        Process proc = Runtime.getRuntime().exec(command);
+
+        // Read the output
+
+        BufferedReader reader =  
+              new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+        String line = "";
+        while((line = reader.readLine()) != null) {
+            System.out.print(line + "\n");
+        }
+
+        proc.waitFor(); 
+        return proc;
+	}
+}
