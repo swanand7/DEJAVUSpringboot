f6d208a327334aea57e3759213db922fff54eb1c
SZAREKAR <swanand.zarekar@capgemini.com>
2022-10-07 14:25:04 +0530

    Transactional added

--- src/main/java/com/example/demo/repo/FingerprintRepo.java
+++ src/main/java/com/example/demo/repo/FingerprintRepo.java
@@ -1,11 +1,14 @@
 package com.example.demo.repo;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.FingerprintEntity;
 
 public interface FingerprintRepo extends JpaRepository<FingerprintEntity, String>{
 
+	@Transactional
 	FingerprintEntity findByFileName(String originalFilename);
 
 }
