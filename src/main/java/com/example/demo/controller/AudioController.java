971967b97362c8ddc48f9957bc603487945542d6
SZAREKAR <swanand.zarekar@capgemini.com>
2022-10-11 18:21:16 +0530

    change to byte array

--- src/main/java/com/example/demo/controller/AudioController.java
+++ src/main/java/com/example/demo/controller/AudioController.java
@@ -6,10 +6,7 @@ import java.nio.file.Files;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.Service.FileService;
 import com.example.demo.entity.FingerprintEntity;
+import com.example.demo.response.AudioResponse;
 import com.example.demo.response.LibraryResponse;
 
 @RestController
@@ -32,7 +30,6 @@ public final class AudioController {
 	@Autowired
 	FileService fileService;
 	private final String FOLDER_PATH = "/home/ubuntu/mp3/";
-
 
 	@GetMapping("/")
 	public String baseApi() {
@@ -55,7 +52,7 @@ public final class AudioController {
 	public ResponseEntity<List<LibraryResponse>> getAllAudio() {
 		return new ResponseEntity<List<LibraryResponse>>(fileService.getAllRecording(), HttpStatus.OK);
 	}
-	
+
 //	@GetMapping("/library/{id}")
 //	public ResponseEntity<?> downloadImageFromFileSystem(@PathVariable Integer id) throws IOException {
 //		byte []audioData=fileService.getAudio(id);
@@ -64,15 +61,26 @@ public final class AudioController {
 //				.body(audioData);
 //
 //	}
+//	@GetMapping("/library/{id}")
+//	public ResponseEntity<Resource> downloadImageFromFileSystem(@PathVariable Integer id) throws IOException {
+//		FingerprintEntity fingerprintEntity=fileService.getAudio(id);
+//		String filePath=FOLDER_PATH+fingerprintEntity.getFileName();
+//		byte []audioData= Files.readAllBytes(new File(filePath).toPath());
+//		return ResponseEntity.ok()
+//				.contentType(MediaType.parseMediaType(fingerprintEntity.getFileType()))
+//				.header(org.springframework.http.HttpHeaders.CONTENT_DISPOSITION, "fingerprintEntity; filename=\""+fingerprintEntity.getFileName()+"\"")
+//				.body(new ByteArrayResource(audioData));
+//
+//	}
 	@GetMapping("/library/{id}")
-	public ResponseEntity<Resource> downloadImageFromFileSystem(@PathVariable Integer id) throws IOException {
+	public ResponseEntity<AudioResponse> downloadImageFromFileSystem(@PathVariable Integer id) throws IOException {
 		FingerprintEntity fingerprintEntity=fileService.getAudio(id);
 		String filePath=FOLDER_PATH+fingerprintEntity.getFileName();
 		byte []audioData= Files.readAllBytes(new File(filePath).toPath());
-		return ResponseEntity.ok()
-				.contentType(MediaType.parseMediaType(fingerprintEntity.getFileType()))
-				.header(org.springframework.http.HttpHeaders.CONTENT_DISPOSITION, "fingerprintEntity; filename=\""+fingerprintEntity.getFileName()+"\"")
-				.body(new ByteArrayResource(audioData));
+		AudioResponse response=new AudioResponse();
+		response.setFile(audioData);
+		return new ResponseEntity<AudioResponse>(response, HttpStatus.OK);
 
 	}
+
 }
---
+++ src/main/java/com/example/demo/response/AudioResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.response;
+
+public class AudioResponse {
+	private byte[] file;
+
+	public byte[] getFile() {
+		return file;
+	}
+
+	public void setFile(byte[] file) {
+		this.file = file;
+	}
+
+}
